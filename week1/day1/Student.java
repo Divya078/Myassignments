46b5164ece4f6aecb022554baffcf653af915cdf
Divya078 <110738731+Divya078@users.noreply.github.com>
2022-08-06 23:54:39 +0530

    Add files via upload
    Week1.Day1

---
+++ week1/day1/Student.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Student {
+
+	String studentName="Divya";
+	int rollNo=1045345;
+	String collegeName="Madras University";
+	int markScored=456;
+	float ccpa=9.12f;
+	
+	public static void main(String[] args) {
+		Student obj =new Student();
+		System.out.println(obj.studentName);
+		System.out.println(obj.rollNo);
+		System.out.println(obj.collegeName);
+		System.out.println(obj.markScored);
+		System.out.println(obj.ccpa);
+	}
+	
+	
+}
+
